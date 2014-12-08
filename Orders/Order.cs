5740c829982d6cacfb0f8ff4cf3bb6646f28f989
Gijs den Besten <gijsbert.den.besten@gmail.com>
2014-12-08 21:03:21 +0100

    Initial commit

---
+++ Orders/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Orders
+{
+    public class Order
+    {
+        public List<OrderItem> OrderItems { get; set; }
+
+        public Order()
+        {
+            OrderItems = new List<OrderItem>();    
+        }
+
+        public double TotalPrice()
+        {
+            var total = 0.0;
+            foreach (var item in OrderItems)
+            {
+                total += item.Number*item.Price;
+            }
+            return total;
+        }
+    }
+}
---
+++ Orders/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orders
+{
+    public class OrderItem
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
