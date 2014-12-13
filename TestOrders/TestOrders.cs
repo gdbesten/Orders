f85e560872cb7ca6eb498e0a64747e074684162a
Gijs den Besten <gijsbert.den.besten@gmail.com>
2014-12-13 18:54:27 +0100

    Test added

--- TestOrders/TestOrders.cs
+++ TestOrders/TestOrders.cs
@@ -24,5 +24,16 @@ namespace TestOrders
 
             Assert.AreEqual(35.0, order.TotalPrice());
         }
+
+        [Test]
+        public void TestTotalPriceOrdersWithThreeOrderItems()
+        {
+            var order = new Order();
+            order.OrderItems.Add(new OrderItem { Number = 1, Name = "Item 1", Price = 10.0 });
+            order.OrderItems.Add(new OrderItem { Number = 5, Name = "Item 2", Price = 5.0 });
+            order.OrderItems.Add(new OrderItem { Number = 4, Name = "Item 3", Price = 25.0 });
+
+            Assert.AreEqual(135.0, order.TotalPrice());
+        }
     }
 }
